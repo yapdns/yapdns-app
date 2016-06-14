3690da5f57e8c54df214289a35b9db03439e46b9
Akshay Aurora <akshayaurora@yahoo.com>
2016-06-14 19:43:25 +0530

    use latest elasticsearch-dsl

--- core/apps.py
+++ core/apps.py
@@ -1,7 +1,21 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.conf import settings
+
+from elasticsearch_dsl.connections import connections
+from elasticsearch.exceptions import NotFoundError
 
 
 class CoreConfig(AppConfig):
     name = 'core'
+    verbose_name = "Core"
+
+    def ready(self):
+        connections.configure(**settings.ES_CONNECTIONS)
+        try:
+            # FIXME: this shouldn't be run before index_data!
+            Question._doc_type.refresh()
+            Answer._doc_type.refresh()
+        except NotFoundError:
+            pass

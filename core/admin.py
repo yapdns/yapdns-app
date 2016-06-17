17abee6e0d7d22393bd65dcaea2fb0d1bac32101
Akshay Aurora <akshayaurora@yahoo.com>
2016-06-17 15:56:40 +0530

    add client model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'secret_key')
+    list_display = ('id', 'name', 'secret_key')
+
+admin.site.register(Client, ClientAdmin)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
+import binascii
+import os
+import uuid
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+def generate_secret_key():
+    return binascii.hexlify(os.urandom(24))
+
+
+@python_2_unicode_compatible
+class Client(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
+    secret_key = models.CharField(max_length=48, default=generate_secret_key, editable=False)
+
+    def __str__(self):
+        return '%s' % (self.name)

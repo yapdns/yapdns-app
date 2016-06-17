b3a33bab521a01123d0ffce7832487610c6cc21f
Akshay Aurora <akshayaurora@yahoo.com>
2016-06-17 13:43:34 +0530

    Add api endpoint to create dns records

--- core/views.py
+++ core/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .search import DnsRecord
+import json
 
-# Create your views here.
+response_map = {
+    200: 'DNS Record create successfully',
+    500: 'Failed to create DNS Record',
+    400: 'Invalid request'
+}
+
+
+def response_from_code(code):
+    response = {
+        'status': code,
+        'message': response_map[code]
+    }
+
+    return JsonResponse(response, status=code)
+
+
+@csrf_exempt
+def create_record(request):
+    if request.method != 'POST':
+        return response_from_code(200)
+
+    body = json.loads(request.body)
+    domain = body['domain']
+    rtype = body['rtype']
+    rdata = body['rdata']
+    ttl = body['ttl']
+
+    client = body['client']
+
+    dns_record = DnsRecord(domain=domain, rtype=rtype, rdata=rdata, ttl=ttl, client=client)
+    dns_record.save()
+    return response_from_code(200)
--- yapdns/urls.py
+++ yapdns/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from core import views
 
 urlpatterns = [
+    url(r'^api/v1/dns', views.create_record, name='api_create'),
     url(r'^admin/', admin.site.urls),
 ]
